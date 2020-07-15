6efccde997d34255877f79fd8d3dc02c70707d63
Danny Berger <dannybmail@gmail.com>
2020-07-15 16:47:41 +0300

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'susanin-client';
+export class AppComponent implements OnInit {
+  emails$: Observable<any>;
+  emailForm = new FormGroup({
+    address: new FormControl('')
+  });
+  private updateSub = new BehaviorSubject(null);
+
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.emails$ = this.updateSub.asObservable()
+      .pipe(
+        switchMap(() => this.httpClient.get('http://localhost:3000'))
+      );
+  }
+
+  deleteEmail(id: string) {
+    this.httpClient.delete(`http://localhost:3000/${id}`)
+      .subscribe(() => this.updateSub.next(null));
+  }
+
+  createEmail() {
+    this.httpClient.post(`http://localhost:3000`, this.emailForm.value)
+      .subscribe(() => this.updateSub.next(null));
+  }
 }
